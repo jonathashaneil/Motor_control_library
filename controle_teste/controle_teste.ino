3f8e10a175fbadf4778febaece59f012c53c71b0
mirellagibson <48492529+mirellagibson@users.noreply.github.com>
2019-04-12 16:59:31 -0300

    Update controle_teste.ino

--- controle_teste/controle_teste.ino
+++ controle_teste/controle_teste.ino
@@ -1,24 +1,43 @@
 #include "controle.h"
+
 float spd = 40;
+int encher = 1;
+int esvaziar = 0;
 Descelularizacao descel;
 
 
 void setup() {
   // put your setup code here, to run once:
-    descel.configuracao();
-    
-descel.tempo = 15; //Em minutos
-descel.x = 3; //Qual motor será utilizado para enchimento
-descel.inicio == "encher";
-descel.final == "esvaziar";
-descel.volume_total = 400; //Em mL
-descel.volume_critico = 50; //Em mL
+  descel.configuracao(); 
+  descel.tempo = 1; //Em minutos
+  descel.x = 3; //Qual motor será utilizado para enchimento
+  descel.inicio = encher;
+  descel.final = esvaziar;
+  descel.volume_total = 100; //Em mL
+  descel.volume_critico = 20; //Em mL
 
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  descel.motor(descel.pressao,descel.tempo,descel.inicio,descel.final);
+   descel.motor(descel.pressao,descel.tempo,descel.inicio,descel.final);
   
+//  descel.ON_MotorPasso();
+//  descel.MotorPasso_Pressao(descel.pressao);
+
+//  Serial.println(descel.tempocritico);
+
+//  descel.serial();
+
+//  descel.ON_MotorDC_2();
+//  delay (2000);
+//  descel.ON_MotorDC_3();
+//  delay (2000);
+//  descel.ON_MotorDC_4();
+//  delay (2000);
+//  descel.OFF_Motores();
+//  delay (10000);
+
 }
